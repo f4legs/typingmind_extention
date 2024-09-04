718cbc408cb9d79ac377851371e8a7a65ceadd06
f4legs <flinkon@gmail.com>
2024-09-05 02:07:41 +0700

    Update change-image.js

--- change-image.js
+++ change-image.js
@@ -1,7 +1,11 @@
+console.log('Extension script loaded');
 const chatTab = document.querySelector('[data-element-id="workspace-tab-chat"]');
+console.log('Chat tab found:', !!chatTab);
 if (chatTab) {
     const iconSpan = chatTab.querySelector('span:first-child');
+    console.log('Icon span found:', !!iconSpan);
     if (iconSpan) {
         iconSpan.textContent = 'New Icon';
+        console.log('Icon text changed');
     }
 }
